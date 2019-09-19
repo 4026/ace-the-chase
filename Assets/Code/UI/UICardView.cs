d53f09cb2f732dc8fa313d01ca448a3210f223c3
Peter Pearson <peetalus@gmail.com>
2019-09-19 11:40:17 +0100

    UICardView now has a setup function to set the card details in the prefab.

--- Assets/Code/UI/UICardView.cs
+++ Assets/Code/UI/UICardView.cs
@@ -8,16 +8,102 @@ namespace AceTheChase.UI
 {
     public class UICardView : MonoBehaviour
     {
-        [SerializeField] private Image m_titleBG;
-        [SerializeField] private Image m_titleHighlight;
-        [SerializeField] private Image m_baseBorder;
 
-        [SerializeField] private Image m_cardTypeIcon;
-        [SerializeField] private Image m_cardImage;
 
-        [SerializeField] private Text m_cardName;
-        [SerializeField] private Text m_cardControlCost;
-        [SerializeField] private Text m_cardEffects;
-        [SerializeField] private Text m_cardFlavourText;
+        [SerializeField]
+        private Image m_cardTypeIcon;
+        [SerializeField]
+        private Image m_cardImage;
+
+
+        [SerializeField]
+        private Image m_baseBorder;
+        [SerializeField]
+        private UIGradient m_borderGradient;
+        [SerializeField]
+        private UIGradient m_titleGradient;
+        [SerializeField]
+        private UIGradient m_titleHighlight;
+
+        [SerializeField]
+        private Text m_cardName;
+        [SerializeField]
+        private Text m_cardControlCost;
+        [SerializeField]
+        private Text m_cardEffects;
+        [SerializeField]
+        private Text m_cardFlavourText;
+
+        [SerializeField]
+        private GameObject m_cardControlCostLayer;
+        [SerializeField]
+        private GameObject m_flavourTextLayer;
+
+        public void Setup(PursuitCard card)
+        {
+            m_cardFlavourText.text = card.Name;
+            m_cardControlCostLayer.SetActive(false);
+
+            UIPalette.CardTypeColourScheme scheme = UIPalette.PursuitCardColours;
+            SetColourScheme(scheme);
+
+            m_cardName.text = card.Name;
+            m_cardEffects.text = card.Description;
+            SetFlavourText(card.FlavourText);
+        }
+
+        public void Setup(PlayerCard card)
+        {
+            m_cardControlCostLayer.SetActive(true);
+            
+            UIPalette.CardTypeColourScheme scheme = UIPalette.GetCardTypeColorScheme(card.CardType);
+            SetColourScheme(scheme);
+
+            m_cardName.text = card.Name;
+            m_cardEffects.text = card.Description;
+            m_cardControlCost.text = card.ControlCost.ToString();
+            SetFlavourText(card.FlavourText);
+        }
+
+        public void Setup(RouteCard card)
+        {
+            m_cardControlCostLayer.SetActive(false);
+            m_flavourTextLayer.SetActive(string.IsNullOrEmpty(card.FlavourText) == false);
+            UIPalette.CardTypeColourScheme scheme = UIPalette.GetCardTypeColorScheme(card.CardType);
+            SetColourScheme(scheme);
+
+            m_cardName.text = card.Name;
+            m_cardEffects.text = card.Description;
+            SetFlavourText(card.FlavourText);
+        }
+
+        private void SetFlavourText(string flavourText)
+        {
+            if (string.IsNullOrEmpty(flavourText) == false)
+            {
+                m_flavourTextLayer.SetActive(true);
+                m_cardFlavourText.text = flavourText;
+            }
+            else
+            {
+                m_flavourTextLayer.SetActive(false);
+            }
+        }
+
+
+        private void SetColourScheme(UIPalette.CardTypeColourScheme scheme)
+        {
+            m_cardTypeIcon.sprite = scheme.Icon;
+            m_baseBorder.color = scheme.BorderGradientBottom;
+
+            m_borderGradient.m_color1 = scheme.BorderGradientTop;
+            m_borderGradient.m_color2 = scheme.BorderGradientBottom;
+
+            m_titleGradient.m_color1 = scheme.TitleColourTop;
+            m_titleGradient.m_color2 = scheme.TitleColourBottom;
+
+            m_titleHighlight.m_color1 = scheme.TitleHighlightTop;
+            m_titleHighlight.m_color2 = scheme.TitleHighlightBottom;
+        }
     }
 }
--- Assets/Code/UI/UIPalette.cs
+++ Assets/Code/UI/UIPalette.cs
@@ -5,15 +5,60 @@ using AceTheChase.GameRules;
 
 namespace AceTheChase.UI
 {
+    [CreateAssetMenu(menuName = "Palettes/UI Palette", fileName = "UIPalette")]
     public class UIPalette : ScriptableObject
     {
-        public struct CardTypeColourScheme
+        public class CardTypeColourScheme
         {
-            public PlayerCardType CardType;
-            public Color TitleColour;
-            public Color TitleHighlightColour;
+            public Color TitleColourTop;
+            public Color TitleColourBottom;
+            public Color TitleHighlightTop;
+            public Color TitleHighlightBottom;
+            public Color BorderGradientTop;
+            public Color BorderGradientBottom;
             public Sprite Icon;
         }
-        [SerializeField] public static Color CardTypeColours;
+
+        public class PlayerCardTypeColourScheme : CardTypeColourScheme
+        {
+            public PlayerCardType CardType;
+        }
+        
+        public class RouteCardTypeColourScheme : CardTypeColourScheme
+        {
+            public RouteCardType CardType;
+        }
+
+        [SerializeField] public static PlayerCardTypeColourScheme[] PlayerCardTypeColours;
+        [SerializeField] public static RouteCardTypeColourScheme[] RouteCardTypeColours;
+        [SerializeField] public static CardTypeColourScheme PursuitCardColours;
+
+        public static CardTypeColourScheme GetCardTypeColorScheme(PlayerCardType type)
+        {
+            foreach (PlayerCardTypeColourScheme scheme in PlayerCardTypeColours)
+            {
+                if (scheme.CardType == type)
+                {
+                    return scheme;
+                }
+            }
+            Debug.LogError("CardTypeColorScheme not found for player card type: " + type.ToString());
+            return new CardTypeColourScheme();
+        }
+
+        public static CardTypeColourScheme GetCardTypeColorScheme(RouteCardType type)
+        {
+            foreach (RouteCardTypeColourScheme scheme in RouteCardTypeColours)
+            {
+                if (scheme.CardType == type)
+                {
+                    return scheme;
+                }
+            }
+            Debug.LogError("CardTypeColorScheme not found for route card type: " + type.ToString());
+            return new CardTypeColourScheme();
+        }
+
+
     }
 }
