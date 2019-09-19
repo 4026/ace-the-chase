96f33b24bdade03eecc1c81b15e79ff468483dd0
Aldo <aldo.curtis@hutchgames.com>
2019-09-19 18:30:19 +0100

    Added buttons to transition between scenes

---
+++ Assets/Code/UI/RulesScreen.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RulesScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadNextScreen()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Build");
+    }
+}
